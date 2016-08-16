c4ac550012223dd657888e2ddf3cd2925ce38cfd
Javed Nissar <javednissar@gmail.com>
2016-08-16 13:08:29 -0400

    redesigned parser to go through directories instead of one big JSON file

--- parser.py
+++ parser.py
@@ -3,8 +3,16 @@ from exceptions import ParseError
 
 def get_lines(file):
     lines = file.readlines()
-    separator_line = lines.index('\n')
-    return lines[:separator_line],lines[separator_line + 1:]
+    import_lines = []
+    text_lines = []
+
+    for line in lines:
+        if line.split(' ')[0] == 'import':
+            import_lines.append(line)
+        elif not line.startswith('#'):
+            text_lines.append(line)
+
+    return import_lines,text_lines
 
 def parse_import_lines(import_lines):
     names_table = {}
@@ -32,19 +40,32 @@ def parse_import_lines(import_lines):
 
     return names_table
 
+def get_dictionary(clarifiers,root_dir,line_number):
+    for index,dirName,subDirList,fileList,clarifier in enumerate(zip(os.walk(root_dir),clarifiers)):
+        if index == len(clarifiers) - 1:
+            for fname in fileList:
+                if clarifier in fname:
+                    if fname.endswith('.json'):
+                        return json.load(open(fname))
+                    else:
+                        raise ParseError('Import was not JSON file',line_number)
+        for index in range(len(subDirList)):
+            if subDirList[index] != clarifier:
+                del subDirList[index]
+    raise ParseError('Import not found',line_number)
+
 def get_dictionaries(names_table,data):
     library_table = {}
     for name in names_table:
         data_name = names_table[name]
         split_name = data_name.split('.')
-        current_dictionary = add.get_dictionary(split_name, data)
+        current_dictionary = get_dictionary(split_name, data)
         if not isinstance(current_dictionary,dict):
             raise TypeError("Name found is not a dictionary")
         library_table[name] = current_dictionary
     return library_table
 
-def parse_text_lines(text_lines,library_table,data_table):
-    library_table["covergen"] = data_table
+def parse_text_lines(text_lines,library_table):
     text = ''.join(text_lines)
     compiled_text = text.format(**library_table)
     return compiled_text
